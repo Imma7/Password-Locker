a5585ab277d668e349a13b1a97282f09141e4352
Imma7 <immamugambi@gmail.com>
2018-05-08 14:47:42 +0300

    Save COntact function

--- credential.py
+++ credential.py
@@ -1,15 +1,15 @@
 class Credential:
     #class property
-    # credential_list = []
+    credential_list = []
 
-    # def __init__ (self, account, username, password):
-    #     self.account = account
-    #     self.username = username
-    #     self.password = password
-    #     Credential.credential_list.append({'account':self.account, 'username':self.username, 'password':self.password})
+    def __init__ (self, account, username, password):
+        self.account = account
+        self.username = username
+        self.password = password
+        Credential.credential_list.append({'account':self.account, 'username':self.username, 'password':self.password})
 
-    # def save(self):
-    #     Credential.credential_list.append(self)
+    def save(self):
+        Credential.credential_list.append(self)
 
     # def delete_account():
     #     pass
