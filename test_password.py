3ee43c4caae72c409176363987f5a41fcdcb1a78
Imma7 <immamugambi@gmail.com>
2018-05-08 11:57:45 +0300

    Import user from User class

--- test_password.py
+++ test_password.py
@@ -1,1 +1,16 @@
 import unittest
+
+from user import User
+
+class TestUser(unittest.TestCase):
+    '''
+    TestUser is a subclass of the parent class
+    unittest.TestCase enables to create test cases
+    '''
+
+    def setUp(self):
+        
+
+
+
+
