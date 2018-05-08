ea3ee7c257fd58aa21ba2b158fed2967e2d2a45c
Imma7 <immamugambi@gmail.com>
2018-05-08 12:16:19 +0300

    Defining Tests

--- test_password.py
+++ test_password.py
@@ -11,10 +11,10 @@ class TestUser(unittest.TestCase):
     def setUp(self): #SetUp method defines instructions to be executed before each test method
         self.new_account = User("GitHub", "Imma", "1234")
 
-    # def test_init(self):
-    #     self.assertEqual(self.new_account.account_name, "GitHub")
-    #     self.assertEqual(self.new_account.username, "Imma")
-    #     self.assertEqual(self.new_account.password, "1234")
+    def test_init(self):
+        self.assertEqual(self.new_account.account_name, "GitHub")
+        self.assertEqual(self.new_account.username, "Imma")
+        self.assertEqual(self.new_account.password, "1234")
 
     
 
