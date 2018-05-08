cd61ca2c66275af625e9e342f8a6ed1d3979e24d
Imma7 <immamugambi@gmail.com>
2018-05-08 14:49:45 +0300

    Added search account function

--- credential.py
+++ credential.py
@@ -20,12 +20,12 @@ class Credential:
         for i in cls.credential_list:
             print(f" || {i['account']} || {i['username']} || {i['password']}")
 
-    # def search_account():
-    #     pass
-    # @classmethod
-    # def delete(cls, acc):
-    #     for a in cls.credential_list:
-    #         if a['account'] == acc:
-    #             cls.credential_list.remove(a)
+    def search_account():
+        pass
+    @classmethod
+    def delete(cls, acc):
+        for a in cls.credential_list:
+            if a['account'] == acc:
+                cls.credential_list.remove(a)
 
 
