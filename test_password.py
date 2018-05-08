4b3ae598abf798c23e2fa974b6c6b5b01a513d67
Imma7 <immamugambi@gmail.com>
2018-05-08 12:14:27 +0300

    Create setUp method

--- test_password.py
+++ test_password.py
@@ -8,8 +8,8 @@ class TestUser(unittest.TestCase):
     unittest.TestCase enables to create test cases
     '''
 
-    # def setUp(self): #SetUp method defines instructions to be executed before each test method
-    #     self.new_account = User("GitHub", "Imma", "1234")
+    def setUp(self): #SetUp method defines instructions to be executed before each test method
+        self.new_account = User("GitHub", "Imma", "1234")
 
     # def test_init(self):
     #     self.assertEqual(self.new_account.account_name, "GitHub")
