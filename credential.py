37de1e3fd67f38aaeb56e5e8fb4b8d774ca7a61f
Imma7 <immamugambi@gmail.com>
2018-05-08 15:06:04 +0300

    Appended credential list

--- credential.py
+++ credential.py
@@ -8,24 +8,24 @@ class Credential:
         self.password = password
         Credential.credential_list.append({'account':self.account, 'username':self.username, 'password':self.password})
 
-    def save(self):
-        Credential.credential_list.append(self)
+    # def save(self):
+    #     Credential.credential_list.append(self)
 
-    def delete_account():
-        pass
-    @classmethod
-    def view(cls):
-        print("Existing Accounts: ")
-        print("|| Account Name ||  username || password ")
-        for i in cls.credential_list:
-            print(f" || {i['account']} || {i['username']} || {i['password']}")
+    # def delete_account():
+    #     pass
+    # @classmethod
+    # def view(cls):
+    #     print("Existing Accounts: ")
+    #     print("|| Account Name ||  username || password ")
+    #     for i in cls.credential_list:
+    #         print(f" || {i['account']} || {i['username']} || {i['password']}")
 
-    def search_account():
-        pass
-    @classmethod
-    def delete(cls, acc):
-        for a in cls.credential_list:
-            if a['account'] == acc:
-                cls.credential_list.remove(a)
+    # def search_account():
+    #     pass
+    # @classmethod
+    # def delete(cls, acc):
+    #     for a in cls.credential_list:
+    #         if a['account'] == acc:
+    #             cls.credential_list.remove(a)
 
 
