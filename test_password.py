4ed8346d5242fcb19e72524b0332372a2986e051
Imma7 <immamugambi@gmail.com>
2018-05-08 12:12:10 +0300

    Create subclass called TestUser

--- test_password.py
+++ test_password.py
@@ -8,8 +8,21 @@ class TestUser(unittest.TestCase):
     unittest.TestCase enables to create test cases
     '''
 
-    def setUp(self):
-        
+    # def setUp(self): #SetUp method defines instructions to be executed before each test method
+    #     self.new_account = User("GitHub", "Imma", "1234")
+
+    # def test_init(self):
+    #     self.assertEqual(self.new_account.account_name, "GitHub")
+    #     self.assertEqual(self.new_account.username, "Imma")
+    #     self.assertEqual(self.new_account.password, "1234")
+
+    
+
+
+
+    
+
+
 
 
 
